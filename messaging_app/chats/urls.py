667ef82ca0788a7562f8ad42cc0996232050e7fc
Winime Awinbugur Azumah <winimeawinbugura@gmail.com>
2025-11-16 22:41:51 +0000

    django projects

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,13 @@
+# messaging_app/chats/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
